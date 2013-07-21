e03ade9a3fec0bc0ee6c3d1c565bac3094f6564b
Andrew McGregor <me@andrewmcgregor.me>
2013-07-21 16:21:10 -0500

    Fix paragraph display + form use after submit

--- public/js/application.js
+++ public/js/application.js
@@ -4,32 +4,40 @@ $(document).ready(function(){
   $('.paragraphs').customSelect();
   $('.word').on('keydown', function(e) {
     if(e.keyCode === 8) {
-      inputWidth = (20 + this.value.length * 12)
-      formWidth = (620 + this.value.length * 13)
+      inputWidth = (20 + this.value.length * 12);
+      formWidth = (620 + this.value.length * 13);
     } else {
-      inputWidth = (40 + this.value.length * 12)
-      formWidth = (620 + this.value.length * 13)
+      inputWidth = (40 + this.value.length * 12);
+      formWidth = (620 + this.value.length * 13);
     }
 
     if(inputWidth < 100) {
-      inputWidth = 100
-      formWidth = 640
+      inputWidth = 100;
+      formWidth = 640;
     }
 
-    $(this).css("width", inputWidth + "px")
-    $('.form').css("width", formWidth + "px")
+    $(this).css("width", inputWidth + "px");
+    $('.form').css("width", formWidth + "px");
   });
 
   $('form').on("submit", function(e) {
-    e.preventDefault()
+    e.preventDefault();
     $.ajax({
       url: "/api/word/"+$('.word').val()+"/paragraphs/"+$('.paragraphs').val(),
       type: 'get',
       success: function(response) {
-        $('.ipsum-container').show().html(response.term);
+        var paragraphs = response.term.split("\n");
+
+        $('.ipsum-container').show().html("");
+
+        $.each(paragraphs, function(index, value) {
+          $('.ipsum-container').append('<p>'+paragraphs[index]+'</p>');
+        });
+
+        $('.form').find('.word, .paragraphs').val("");
       },
       error: function() {
-        console.log("fail")
+        console.log("fail");
       }
     })
   })
