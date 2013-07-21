b2240d275e275225ac0f70e1b3f902e2c5092d59
Andrew McGregor <me@andrewmcgregor.me>
2013-07-21 17:23:47 -0500

    debugging

--- wiki_client.rb
+++ wiki_client.rb
@@ -7,12 +7,17 @@ module WikiClient
   extend self
 
   def build api_request = {}
+    puts api_request
     paragraph_count = api_request[:paragraphs]
     search_term = api_request[:term]
     ipsum_seed_data = {}
     ipsum_seed_data[:paragraphs] = paragraph_count
+    puts search_term
     search_term_string = search_term.gsub(' ','_')
-    ipsum_seed_text = WikiClient.wiki_clean ( Wikipedia.find( search_term_string ).sanitized_content )
+    search_result = Wikipedia.find( search_term_string ).sanitized_content
+    search_result = Wikipedia.find('bacon').sanitized_content unless search_result && search_result.length > 0
+    ipsum_seed_text = WikiClient.wiki_clean search_result
+    # ipsum_seed_text = WikiClient.wiki_clean ( Wikipedia.find( search_term_string ).sanitized_content )
     ipsum_seed_data[:text_from_source] = ipsum_seed_text
     ipsum_seed_data
   end
