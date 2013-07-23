8cb5de480606e6120c6f78a84652030c6357dce3
JEFF <justaboutjeff@gmail.com>
2013-07-22 23:54:02 -0500

    Adds new_relic gem for server ping and statistics

--- server.rb
+++ server.rb
@@ -4,17 +4,20 @@ require './helpers'
 require './generator'
 require './wiki_client'
 
+configure :production do
+  require 'newrelic_rpm'
+end
+
 get '/' do
   erb :index
 end
 
 get '/api/term/:term/paragraphs/:paragraphs' do
-  ipsum_seed_data = WikiClient.build(term: params[:term], 
-                          paragraphs: params[:paragraphs])
+  ipsum_seed_data = WikiClient.build(term: params[:term],
+                                     paragraphs: params[:paragraphs])
 
   ipsumText = Generator.make_multiple_paragraphs(ipsum_seed_data)
 
   content_type :json
   { 'anyIpsum' => ipsumText}.to_json
-
 end
